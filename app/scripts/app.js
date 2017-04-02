c1aa0bb78c703c395f1a3460d0d84da102837d9e
Ivan <Ivan Sakhman>
2017-04-02 10:12:08 +0300

    added parallax

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,4 +3,9 @@ import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+    $('.footer__up').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
 });
