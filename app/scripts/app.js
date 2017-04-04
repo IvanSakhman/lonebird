ff4463e3bf2d5942c6d891579dd86ca82c5a311e
Ivan <Ivan Sakhman>
2017-04-04 20:02:42 +0300

    added about page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,19 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	svg4everybody();
+    svg4everybody();
 
-    $('.footer__up').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 1000);
+    $('.footer__up').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
         return false;
     });
+
+    $('.menu__open').click(function () {
+        $('.menu__overlay').css("width", "100%");
+    });
+
+    $('.menu__close').click(function () {
+        $('.menu__overlay').css("width", "0%");
+    });
+
 });
